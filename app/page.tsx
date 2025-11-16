7e3a6c88c69146feb2ec3a5f43087d25fee33955
yoseph0524 <yoseph0524@g.ucla.edu>
2025-11-15 17:45:51 -0800

    List and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-gray-300 h-screen flex sm:bg-red-100 md:bg-green-100 lg:cyan-100 items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 ring ring-transparent transition-shadow *:outline-none has-[:invalid]:ring-red-100 has-[:invalid]:ring">
-        <input
-          className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shado placeholder:drop-shadow invalid:focus:ring-red-500 peer "
-          type="email"
-          placeholder="Search here..."
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-300 to-purple-400 peer-invalid:bg-red-300">
-          Search
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3 ">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 *:animate-pulse">
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className=" w-40 h-4 rounded-full bg-gray-400" />
+            <span className=" w-20 h-4 rounded-full bg-gray-400" />
+          </div>
+        ))}
       </div>
     </main>
   );
