f4098e347c52eca495a1b650b848528433ba3b5b
yoseph0524 <yoseph0524@g.ucla.edu>
2025-11-15 18:09:25 -0800

    Group modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-gray-300 h-screen flex sm:bg-red-100 md:bg-green-100 lg:cyan-100 items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3 ">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4 ">
         {["Nico", "Me", "You", "Yourself"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 *:animate-pulse">
+          <div key={index} className="flex items-center group gap-5">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className=" w-40 h-4 rounded-full bg-gray-400" />
-            <span className=" w-20 h-4 rounded-full bg-gray-400" />
+            <span className=" text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
+              {person}
+            </span>
+            <div className="size-6  bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
           </div>
         ))}
       </div>
