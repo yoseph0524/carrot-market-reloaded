66e4303de545b7856e12c1e7802b7c5663088ea7
yoseph0524 <yoseph0524@g.ucla.edu>
2025-11-17 23:18:51 -0800

    Zod

---
+++ app/create-account/actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+  console.log(data);
+}
--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,44 +1,40 @@
+"use client";
+
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import FormInput from "../../components/form-input";
 import FormButton from "../../components/form-btn";
 import SocialLogin from "../../components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">Hello!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={dispatch} className="flex flex-col gap-3">
         <FormInput
-          name="password"
+          name="username"
           type="text"
           placeholder="Username"
           required
-          errors={[]}
         />
-        <FormInput
-          name="password"
-          type="email"
-          placeholder="Email"
-          required
-          errors={[]}
-        />
+        <FormInput name="email" type="email" placeholder="Email" required />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[]}
         />
         <FormInput
-          name="password"
+          name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
-          errors={[]}
         />
         <FormButton text="Create account" />
       </form>
