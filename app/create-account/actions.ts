9297600535ec53dc008e6dcb22dedaf9dd548c72
yoseph0524 <yoseph0524@g.ucla.edu>
2025-11-18 00:18:22 -0800

    refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,29 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        error: (issue) =>
+          issue.input === undefined
+            ? "Where is my username???"
+            : "Not a string",
+      })
+      .min(3, "Way too short!!!")
+      .max(10, "That is too looooong!")
+      .refine(
+        (username) => !username.includes("potato"),
+        "No potatoes allowed!"
+      ),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(({ password, confirm_password }) => password === confirm_password, {
+    error: "Both passwords should be the same",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
