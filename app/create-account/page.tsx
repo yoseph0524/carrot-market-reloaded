bf075dc5bbaeaf72ac9b21d08b956dd637914928
yoseph0524 <yoseph0524@g.ucla.edu>
2025-11-28 23:55:59 -0800

    Refactor

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,51 +1,53 @@
 "use client";
 
-import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
-import FormInput from "../../components/form-input";
-import FormButton from "../../components/form-btn";
-import SocialLogin from "../../components/social-login";
+import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
 import { createAccount } from "./actions";
+import Input from "../components/input";
+import Button from "../components/button";
 
 export default function CreateAccount() {
   const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">Hello!</h1>
+        <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <FormInput
+        <Input
           name="username"
           type="text"
           placeholder="Username"
           required
           errors={state?.fieldErrors.username}
+          minLength={3}
+          maxLength={10}
         />
-        <FormInput
+        <Input
           name="email"
           type="email"
           placeholder="Email"
           required
           errors={state?.fieldErrors.email}
         />
-        <FormInput
+        <Input
           name="password"
           type="password"
           placeholder="Password"
+          minLength={4}
           required
           errors={state?.fieldErrors.password}
         />
-        <FormInput
+        <Input
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
+          minLength={4}
           errors={state?.fieldErrors.confirm_password}
         />
-        <FormButton text="Create account" />
+        <Button text="Create account" />
       </form>
       <SocialLogin />
     </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,28 @@
 "use client";
 
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
-import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
 import { useFormState } from "react-dom";
 import { handleForm } from "./actions";
+import Input from "@/components/input";
+import Button from "@/components/button";
 
 export default function LogIn() {
   const [state, action] = useFormState(handleForm, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">Hello!</h1>
+        <h1 className="text-2xl">Hello</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <FormInput name="email" type="email" placeholder="Email" required />
-        <FormInput
+        <Input name="email" type="email" placeholder="Email" required />
+        <Input
           name="password"
           type="password"
           placeholder="Password"
           required
         />
-
-        <FormButton text="Create account" />
+        <Button text="Log in" />
       </form>
       <SocialLogin />
     </div>
