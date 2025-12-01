53b64cc2f950b016a52c508bb3c00859e285ad88
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-01 00:20:55 -0800

    Prisma Client

--- lib/db.ts
+++ lib/db.ts
@@ -2,15 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const user = await db.user.create({
-    data: {
-      username: "test",
-    },
-  });
-  console.log(user);
-}
-
-test();
-
 export default db;
