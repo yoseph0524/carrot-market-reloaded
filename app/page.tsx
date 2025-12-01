1bc624244316a5a5df09ae63f233b5f2e60a28e5
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-01 00:16:23 -0800

    Prisma Client

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import "../lib/db";
 
 export default function Home() {
   return (
--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
+
+async function test() {
+  const user = await db.user.create({
+    data: {
+      username: "test",
+    },
+  });
+  console.log(user);
+}
+
+test();
+
 export default db;
