7cf4b8a75a53138d9749d790a75eeb92e868d7bc
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-01 22:52:01 -0800

    Email Login

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -10,6 +10,7 @@ import { z } from "zod";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import getSession from "lib/session";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -93,20 +94,15 @@ export async function createAccount(prevState: any, formData: FormData) {
       data: {
         username: result.data.username,
         email: result.data.email,
-        password: result.data.password,
+        password: hashedPassword,
       },
       select: { id: true },
     });
     console.log(user);
 
-    // log the user in
-    const cookie = await getIronSession(cookies(), {
-      cookieName: "delicious-karrot",
-      password: process.env.COOKIE_PASSWORD!,
-    });
-    //@ts-ignore
-    cookie.id = user.id;
-    await cookie.save();
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
     redirect("/profile");
   }
 }
