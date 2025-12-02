1e028de30509666a14d1811fc3fbeaccbb3508b8
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-01 23:42:22 -0800

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import getSession from "lib/session";
 import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
