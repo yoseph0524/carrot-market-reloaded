86eeafc6564c8ab6f663580f3157d01d73753efb
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-03 15:19:21 -0800

    SMS Token

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,7 +1,9 @@
-"use client";
+"use server";
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import db from "lib/db";
+import crypto from "crypto";
 
 const phoneSchema = z
   .string()
@@ -17,6 +19,23 @@ interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 export async function smsLogIn(prevState: ActionState, formData: FormData) {
   const phone = formData.get("phone");
   const token = formData.get("token");
@@ -28,6 +47,31 @@ export async function smsLogIn(prevState: ActionState, formData: FormData) {
         error: result.error.flatten(),
       };
     } else {
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+
       return {
         token: true,
       };
