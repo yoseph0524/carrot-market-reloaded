83f6deece94b5aefdbcadf999b71d479e408e1ca
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-05 21:16:46 -0800

    Product Component

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,8 @@ export async function middleware(request: NextRequest) {
     }
   }
 }
-
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*(?<!\\.jpg)(?<!\\.jpeg)(?<!\\.png)(?<!\\.gif)(?<!\\.webp)$)",
+  ],
 };
