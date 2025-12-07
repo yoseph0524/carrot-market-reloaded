7f2b61cbcd8e78b3a6c5a31bf5d7d921112a71c7
yoseph0524 <yoseph0524@g.ucla.edu>
2025-12-06 16:54:29 -0800

    Upload URLs

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -24,11 +24,7 @@ export async function uploadProduct(_: any, formData: FormData) {
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  if (data.photo instanceof File) {
-    const photoData = await data.photo.arrayBuffer();
-    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
-    data.photo = `/${data.photo.name}`;
-  }
+
   const result = productSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
@@ -55,4 +51,18 @@ export async function uploadProduct(_: any, formData: FormData) {
       //redirect("/products")
     }
   }
+}
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
 }
